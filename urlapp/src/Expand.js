68c11ca9f68efb5f715d79e525a4b10cea728995
niloofarmotamed <niloofar.motamed@gmail.com>
2017-12-18 20:24:39 +0200

    add Shorten component

---
+++ urlapp/src/Expand.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Expand extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Expand;
---
+++ urlapp/src/Shorten.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Shorten extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Here is your new url, short and ready to use:</h3>
+            </div>
+        )
+    }
+}
+
+export default Shorten;

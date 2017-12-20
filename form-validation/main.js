090be6bf8eda3415741e2ebf37ca6af054ec2159
niloofarmotamed <niloofar.motamed@gmail.com>
2017-12-20 23:38:22 +0200

    add user input tp the checkbox section and user can tick or delete it

--- form-validation/main.js
+++ form-validation/main.js
@@ -1,29 +1,40 @@
 const deletItems = document.getElementsByClassName("delete");
 const deleteArray = Array.from(deletItems);
-const parentEl = Array.from(document.getElementsByClassName("check"))
-deleteArray.forEach((box)=>{
-    box.addEventListener('click',(e)=>{
-        e.preventDefault(e);
-        box.parentElement.removeChild(box.previousElementSibling) 
-    })
-})
-function addInput(){
-const newInput = document.createElement("span");
-const labelVal =newInput.appendChild(document.createElement("label"))
-console.log(labelVal)
-const inputValue =document.querySelector('.extraInput')
-labelVal.innerText = inputValue.value
-document.body.insertBefore(newInput,document.body.firstChild)
-console.log(inputValue.value)
+const parentEl = Array.from(document.getElementsByClassName("check"));
+deleteArray.forEach(box => {
+  box.addEventListener("click", e => {
+    e.preventDefault(e);
+    box.parentElement.removeChild(box.previousElementSibling);
+  });
+});
+function addInput() {
+  const newInput = document.createElement("span");
+  const inputValue = document.querySelector(".extraInput");
+  if (inputValue.value === "") {
+    return;
+  }
+  const checkInput = newInput.appendChild(document.createElement("input"));
+  const labelVal = newInput.appendChild(document.createElement("label"));
+  const newBut = newInput.appendChild(document.createElement("button"));
+  const parentElement = document.querySelector(".exp");
+  newBut.innerText = "X";
+  newBut.classList.add("delete");
+  newBut.addEventListener("click", e => {
+    e.preventDefault();
+    parentElement.removeChild(newInput);
+  });
+  labelVal.setAttribute("for", inputValue.value);
+  labelVal.innerText = inputValue.value;
+  newInput.classList.add("check");
+  checkInput.setAttribute("name", "check-answer1");
+  checkInput.setAttribute("type", "checkbox");
+  checkInput.setAttribute("id", inputValue.value);
+
+  const sis = document.querySelector("#sister");
+  parentElement.insertBefore(newInput, sis.nextSibling);
 }
-const add = document.querySelector('#add');
-add.addEventListener("click",(e)=>{
-e.preventDefault(e);
-addInput();
-
-
-
- 
-
-})
-//console.log(add)
+const add = document.querySelector("#add");
+add.addEventListener("click", e => {
+  e.preventDefault(e);
+  addInput();
+});
